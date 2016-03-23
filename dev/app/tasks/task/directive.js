32ca4cece09d3044b8cc19e61ecb9f402ba05b1e
pnmougel <pnmougel@gmail.com>
2016-03-23 20:04:15 +0100

    Initialize the progress bar

--- dev/app/tasks/task/directive.js
+++ dev/app/tasks/task/directive.js
@@ -6,24 +6,27 @@ App.directive('task', function (Tasks, $interval, moment) {
       update: '='
     },
     link: function (scope) {
-      scope.item.percentage = 0;
-      var startedAt = moment(scope.item.createdAt).unix()
-      moment.locale('en')
+      var updateTasks = function () {
+        Tasks.state({id: scope.item._id}).$promise.then(function (item) {
+          var now = moment().unix();
+          var secondToFinish = (now - startedAt) / item.percentage;
+          var ending = moment().add(secondToFinish, 's');
+          scope.percentage = item.percentage;
+          scope.doing = item.doing;
+          scope.status = item.status;
+          scope.remaining = ending.fromNow();
+          if(item.status !== "Running") {
+            scope.update();
+          }
+        });
+      };
+      updateTasks();
+
+      var startedAt = moment(scope.item.createdAt).unix();
+      moment.locale('en');
+
       if(scope.item.status === "Running") {
-        $interval(function () {
-          Tasks.state({id: scope.item._id}).$promise.then(function (item) {
-            var now = moment().unix()
-            var secondToFinish = (now - startedAt) / item.percentage;
-            var ending = moment().add(secondToFinish, 's');
-            scope.item.percentage = item.percentage;
-            scope.item.doing = item.doing;
-            scope.item.status = item.status;
-            scope.item.remaining = ending.fromNow();
-            if(item.status !== "Running") {
-              scope.update();
-            }
-          });
-        }, 2000);
+        $interval(updateTasks, 2000);
       }
       scope.stop = function () {
         Tasks.stop({id: scope.item._id}, {id: scope.item._id}).$promise.then(function () {
