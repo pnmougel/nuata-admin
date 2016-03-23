daa1ead3b91a1da57ab29f74a608bff66694e810
pnmougel <pnmougel@gmail.com>
2016-03-23 20:03:33 +0100

    Added Html5 mode

--- dev/app/config/routes.js
+++ dev/app/config/routes.js
@@ -3,62 +3,47 @@
  * App routes configuration
  =========================================================*/
 
-App.config(['$stateProvider', '$locationProvider', '$urlRouterProvider',
-    function ($stateProvider, $locationProvider, $urlRouterProvider) {
-        'use strict';
+App.config(function ($stateProvider, $locationProvider, $urlRouterProvider, Html5Mode) {
+  'use strict';
 
-        // Set the following to true to enable the HTML5 Mode
-        // You may have to set <base> tag in index and a routing configuration in your server
-        //$locationProvider.html5Mode(true);
+  // Set the following to true to enable the HTML5 Mode
+  // You may have to set <base> tag in index and a routing configuration in your server
+  $locationProvider.html5Mode(Html5Mode);
 
-        // default route
-        // $urlRouterProvider.otherwise('app/dashboard');
-        $urlRouterProvider.otherwise('app/items');
+  // default route
+  $urlRouterProvider.otherwise('app/tasks');
 
-        $stateProvider
-            .state('app', {
-                abstract: true,
-                url: '/app',
-                templateUrl: 'app/menu/view.html',
-                controller: 'MenuCtrl'
-            })
-            .state('app.dashboard', {
-                url: '/dashboard',
-                templateUrl: 'app/dashboard/view.html',
-                controller: 'DashboardCtrl'
-            })
-            .state('app.items', {
-                url: '/items',
-                templateUrl: 'app/items/view.html',
-                controller: 'ItemsCtrl',
-                params: { type: null }
-            })
-            .state('app.item', {
-                url: '/item',
-                templateUrl: 'app/item/view.html',
-                controller: 'ItemCtrl',
-                params: { id: null, type: null }
-            })
-            .state('app.attributes', {
-                url: '/attributes',
-                templateUrl: 'app/attributes/view.html',
-                controller: 'AttributesCtrl'
-            })
-            .state('app.viewers', {
-                url: '/viewers',
-                templateUrl: 'app/viewers/view.html',
-                controller: 'ViewersCtrl'
-            })
-            .state('app.tasks', {
-                url: '/tasks',
-                templateUrl: 'app/tasks/view.html',
-                controller: 'TasksCtrl',
-                params: {status: null}
-            })
-            .state('app.updates', {
-                url: '/updates',
-                templateUrl: 'app/updates/view.html',
-                controller: 'UpdateCtrl',
-                params: { name: null }
-            });
-    }]);
+  $stateProvider
+      .state('login', {
+        url: '/login',
+        templateUrl: 'app/login/view.html',
+        controller: 'LoginCtrl'
+      })
+      .state('app', {
+        abstract: true,
+        url: '/app',
+        templateUrl: 'app/app/view.html',
+        controller: 'AppCtrl'
+      })
+      .state('app.dashboard', {
+        url: '/dashboard',
+        templateUrl: 'app/dashboard/view.html',
+        controller: 'DashboardCtrl'
+      })
+      .state('app.attributes', {
+        url: '/attributes',
+        templateUrl: 'app/attributes/view.html',
+        controller: 'AttributesCtrl'
+      })
+      .state('app.viewers', {
+        url: '/viewers',
+        templateUrl: 'app/viewers/view.html',
+        controller: 'ViewersCtrl'
+      })
+      .state('app.tasks', {
+        url: '/tasks',
+        templateUrl: 'app/tasks/view.html',
+        controller: 'TasksCtrl',
+        params: {status: null}
+      });
+});
